ff4234f1c415986a28f5a2e851fd5a22f76d42e8
Matthew Mott <orbweaver3d@gmail.com>
2025-12-23 14:33:38 +0000

    Reduce exceptions thrown by map format classes
    Instead of canLoad() calling assertNextToken() and catching an exception
    if the token does not match, just call nextToken() and examine the
    value.

--- radiantcore/map/format/Quake4MapFormat.cpp
+++ radiantcore/map/format/Quake4MapFormat.cpp
@@ -13,83 +13,84 @@ namespace map
 // RegisterableModule implementation
 std::string Quake4MapFormat::getName() const
 {
-	static std::string _name("Quake4MapLoader");
-	return _name;
+    static std::string _name("Quake4MapLoader");
+    return _name;
 }
 
 StringSet Quake4MapFormat::getDependencies() const
 {
-	static StringSet _dependencies;
+    static StringSet _dependencies;
 
-	if (_dependencies.empty())
-	{
-		_dependencies.insert(MODULE_MAPFORMATMANAGER);
-	}
+    if (_dependencies.empty())
+    {
+        _dependencies.insert(MODULE_MAPFORMATMANAGER);
+    }
 
-	return _dependencies;
+    return _dependencies;
 }
 
 void Quake4MapFormat::initialiseModule(const IApplicationContext& ctx)
 {
-	// Register ourselves as map format for maps, regions and prefabs
-	GlobalMapFormatManager().registerMapFormat("map", shared_from_this());
-	GlobalMapFormatManager().registerMapFormat("reg", shared_from_this());
-	GlobalMapFormatManager().registerMapFormat("pfb", shared_from_this());
+    // Register ourselves as map format for maps, regions and prefabs
+    GlobalMapFormatManager().registerMapFormat("map", shared_from_this());
+    GlobalMapFormatManager().registerMapFormat("reg", shared_from_this());
+    GlobalMapFormatManager().registerMapFormat("pfb", shared_from_this());
 }
 
 void Quake4MapFormat::shutdownModule()
 {
-	// Unregister now that we're shutting down
-	GlobalMapFormatManager().unregisterMapFormat(shared_from_this());
+    // Unregister now that we're shutting down
+    GlobalMapFormatManager().unregisterMapFormat(shared_from_this());
 }
 
 const std::string& Quake4MapFormat::getMapFormatName() const
 {
-	static std::string _name = "Quake 4";
-	return _name;
+    static std::string _name = "Quake 4";
+    return _name;
 }
 
 const std::string& Quake4MapFormat::getGameType() const
 {
-	static std::string _gameType = "quake4";
-	return _gameType;
+    static std::string _gameType = "quake4";
+    return _gameType;
 }
 
 IMapReaderPtr Quake4MapFormat::getMapReader(IMapImportFilter& filter) const
 {
-	return IMapReaderPtr(new Quake4MapReader(filter));
+    return IMapReaderPtr(new Quake4MapReader(filter));
 }
 
 IMapWriterPtr Quake4MapFormat::getMapWriter() const
 {
-	return IMapWriterPtr(new Quake4MapWriter);
+    return IMapWriterPtr(new Quake4MapWriter);
 }
 
 bool Quake4MapFormat::allowInfoFileCreation() const
 {
-	// allow .darkradiant files to be saved
-	return true;
+    // allow .darkradiant files to be saved
+    return true;
 }
 
 bool Quake4MapFormat::canLoad(std::istream& stream) const
 {
-	// Instantiate a tokeniser to read the first few tokens
-	parser::BasicDefTokeniser<std::istream> tok(stream);
+    // Instantiate a tokeniser to read the first few tokens
+    parser::BasicDefTokeniser<std::istream> tok(stream);
 
-	try
-	{
-		// Require a "Version" token
-		tok.assertNextToken("Version");
+    try
+    {
+        // Require a "Version" token
+        if (const std::string token = tok.nextToken(); token != "Version")
+            return false;
 
-		// Require specific version, return true on success
-		return (std::stof(tok.nextToken()) == MAP_VERSION_Q4);
-	}
-	catch (parser::ParseException&)
-	{}
-	catch (std::invalid_argument&)
-	{}
+        // Require specific version, return true on success
+        const std::string verTok = tok.nextToken();
+        float version;
+        return string::tryConvertToFloat(verTok, version) && version == MAP_VERSION_Q4;
+    }
+    catch (parser::ParseException&)
+    {}
 
-	return false;
+    return false;
 }
 
 module::StaticModuleRegistration<Quake4MapFormat> q4MapModule;
